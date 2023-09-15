905b50f996d6a7a31c71b42fbd034394bd837fef
lazarevkristijan <lazarevkristijan@gmail.com>
2023-09-15 20:31:52 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import "./index.css"
+
 function App() {
   return (
-    <div>
-      <p>Habit</p>
+    <div className="text-center text-[50px]">
+      <p>Rituals</p>
+      <p>Where this journey begins</p>
+      <p className="text-[20px]">8:30PM, 15.09.2023</p>
     </div>
   )
 }
