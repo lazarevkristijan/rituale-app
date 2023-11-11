5b9f593f2b8109c28bd0760671825bf01e8d6ba1
lazarevkristijan <lazarevkristijan@gmail.com>
2023-11-11 13:50:16 +0200

    cors credentials = true

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({ credentials: true }))
 
 const server = app.listen(port, () =>
   console.log(`Rituale db is listening on port ${port}!`)
