8fcd8319f42e80189fd14b461c4ff98ddc17825b
lazarevkristijan <lazarevkristijan@gmail.com>
2023-11-12 18:41:08 +0200

    rm slash in cors origin

--- express-hello-world-main/app.js
+++ express-hello-world-main/app.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 3001
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
