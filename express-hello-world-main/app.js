06e1263b806cba9ba0daf37d740ed887e72126a1
lazarevkristijan <lazarevkristijan@gmail.com>
2023-11-12 18:30:42 +0200

    test diff origin of cors

--- express-hello-world-main/app.js
+++ express-hello-world-main/app.js
@@ -5,9 +5,27 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT || 3001
 
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://your-frontend.com")
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   )
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   )
+//   res.setHeader("Access-Control-Allow-Credentials", true)
+//   res.setHeader("Access-Control-Allow-Private-Network", true)
+//   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+//   res.setHeader("Access-Control-Max-Age", 7200)
+
+//   next()
+// })
+
 app.use(
   cors({
-    origin: "https://api.rituale.digital",
+    origin: "http://localhost:5173/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
