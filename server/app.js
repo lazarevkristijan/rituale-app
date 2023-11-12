cf4acad17fd96c60a02dadbf632458da47e172d6
lazarevkristijan <lazarevkristijan@gmail.com>
2023-11-12 15:28:40 +0200

    debugging app.js reset

--- server/app.js
+++ server/app.js
@@ -1,34 +1,15 @@
-import express from "express"
-import sql from "./db.js"
-import cors from "cors"
-
+const express = require("express")
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(cors())
-
-const server = app.listen(port, () =>
-  console.log(`Rituale db is listening on port ${port}!`)
-)
-
-server.keepAliveTimeout = 7200 * 1000
-server.headersTimeout = 7200 * 1000
-
-app.get("/api/users", async (req, res) => {
-  try {
-    const users = await sql`
-    SELECT *
-    FROM users
-    `
-    res.json(users)
-  } catch (error) {
-    console.error(error.message)
-    console.error("Error fetching users: ", error)
-    res.status(500).json({ error: "Internal Server Error" })
-  }
-})
+const port = process.env.PORT || 3001
 
 app.get("/", (req, res) => res.type("html").send(html))
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+)
+
+server.keepAliveTimeout = 120 * 1000
+server.headersTimeout = 120 * 1000
 
 const html = `
 <!DOCTYPE html>
