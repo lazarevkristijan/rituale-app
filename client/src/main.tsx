099e2861dcfe4ce1097e3ff51d9a611897e4dd8d
lazarevkristijan <lazarevkristijan@gmail.com>
2023-11-15 10:13:28 +0200

    fix missing browserRouter in main.jsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
+import { BrowserRouter } from "react-router-dom"
 import { store } from "./Store.ts"
 import { Provider } from "react-redux"
 import { QueryClient, QueryClientProvider } from "react-query"
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>
