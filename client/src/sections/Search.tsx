01f9f560fce6af32c89386489fa95d2c7fea9e5c
lazarevkristijan <lazarevkristijan@gmail.com>
2023-12-20 14:37:08 +0200

    better search func

--- client/src/sections/Search.tsx
+++ client/src/sections/Search.tsx
@@ -1,23 +1,26 @@
-import { Box, TextField, Typography } from "@mui/material"
+import { Box, Button, Grid, TextField, Typography } from "@mui/material"
 import axios from "axios"
 import { useState } from "react"
 import MainLoadingScreen from "../skeletons/MainLoadingScreen"
 import { useQuery } from "react-query"
 import { UserTypes } from "../Types"
+import { useNavigate } from "react-router-dom"
 const Search = () => {
   const [searchValue, setSearchValue] = useState("")
-
+  const navigate = useNavigate()
   const getUsers = async () => {
     const res = await axios.get("http://localhost:5432/users")
     return res.data
   }
-  const { data, isLoading, error } = useQuery("users", getUsers)
+  const { data, isLoading } = useQuery("users", getUsers)
 
   return (
     <Box>
       <Typography variant="h2">Search users</Typography>
       <TextField
         fullWidth
+        label="User"
+        placeholder="Who?"
         value={searchValue}
         onChange={(e) => {
           setSearchValue(e.target.value)
@@ -26,7 +29,77 @@ const Search = () => {
       {isLoading ? (
         <MainLoadingScreen />
       ) : (
-        data.map((user: UserTypes) => user.first_name)
+        <Grid
+          gap={2}
+          sx={{ display: "flex" }}
+        >
+          {data
+            .filter(
+              (user: UserTypes) =>
+                user.first_name
+                  .toLowerCase()
+                  .includes(searchValue.toLowerCase()) ||
+                user.last_name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((user: UserTypes) => {
+              let pfpURL
+              if (user?.profile_picture) {
+                const pfpData = JSON.parse(user?.profile_picture)
+                pfpURL = pfpData.url
+              }
+
+              return (
+                <Box
+                  sx={{
+                    width: 300,
+                    height: 200,
+                    bgcolor: "primary.dark",
+                    borderRadius: 2,
+                    p: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Box>
+                    <Box
+                      component="img"
+                      src={pfpURL}
+                      width={50}
+                      height={50}
+                      borderRadius={20}
+                      sx={{ border: "3px solid black", bgcolor: "#fff" }}
+                    />
+                    <Typography component="span">
+                      Full name: {user.first_name} {user.last_name}
+                    </Typography>
+                    <Typography>Country: {user.country}</Typography>
+                    <Typography>
+                      Main focus:{" "}
+                      {user.priority_category_1 &&
+                        user.priority_category_1 + ", "}
+                      {user.priority_category_2 &&
+                        user.priority_category_2 + ", "}{" "}
+                      {user.priority_category_2 && user.priority_category_3}
+                      {!user.priority_category_1 &&
+                        !user.priority_category_2 &&
+                        !user.priority_category_3 &&
+                        "None"}
+                    </Typography>
+                  </Box>
+                  <Box sx={{ display: "flex", justifyContent: "end" }}>
+                    <Button
+                      onClick={() => {
+                        navigate(`/user/${user.id}`)
+                      }}
+                    >
+                      view
+                    </Button>
+                  </Box>
+                </Box>
+              )
+            })}
+        </Grid>
       )}
     </Box>
   )
--- client/src/sections/UserProfile.tsx
+++ client/src/sections/UserProfile.tsx
@@ -2,12 +2,12 @@ import axios from "axios"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate, useParams } from "react-router-dom"
 import { changeLocation } from "../features/bottomNav/bottomNavSlice"
-import { useEffect, useState } from "react"
-import { UserTypes } from "../Types"
 import { Box, Button, Chip, Stack, Tooltip, Typography } from "@mui/material"
 import { RootState } from "../Store"
 import React from "react"
 import { countryShorthands } from "../constants"
+import { useQuery } from "react-query"
+import MainLoadingScreen from "../skeletons/MainLoadingScreen"
 
 const UserProfile = () => {
   const dispatch = useDispatch()
@@ -16,23 +16,20 @@ const UserProfile = () => {
 
   const { id } = useParams()
 
-  const user = useSelector((state: RootState) => state.session.user)
   const darkTheme = useSelector((state: RootState) => state.settings.colorTheme)
   const completedHabits = useSelector(
     (state: RootState) => state.completedHabits
   )
 
-  const [userData, setUserData] = useState<UserTypes | null>(null)
+  const getExternalUserData = async () => {
+    const res = await axios.get(`http://localhost:5432/user/${id}`)
+    return res.data
+  }
 
-  console.log("user data: ", userData)
-  console.log("user: ", user)
-  useEffect(() => {
-    axios.get(`http://localhost:5432/user/${id}`).then((response) => {
-      setUserData(response.data)
-      console.log(response.data)
-    })
-  }, [id])
-
+  const { data, isLoading } = useQuery(
+    "external-user-profile",
+    getExternalUserData
+  )
   const displayBio = (bio: string | null | undefined) => {
     if (bio === null || bio === undefined) {
       return "NO BIO"
@@ -45,127 +42,134 @@ const UserProfile = () => {
     ))
     return formattedBio
   }
-
   let pfpURL
-  if (userData?.profile_picture) {
-    const pfpData = JSON.parse(userData?.profile_picture)
+  if (data) {
+    const pfpData = JSON.parse(data.profile_picture)
     pfpURL = pfpData.url
   }
 
   return (
-    <>
-      <Typography variant="h3">{userData?.first_name}'s Profile</Typography>
+    <Box>
+      {isLoading ? (
+        <MainLoadingScreen />
+      ) : (
+        <>
+          <Typography variant="h3">{data.first_name}'s Profile</Typography>
 
-      <Box
-        sx={{
-          bgcolor: `primary.${darkTheme ? "dark" : "light"}`,
-          borderRadius: 1,
-          p: 1,
-          mb: 2,
-          display: "flex",
-        }}
-      >
-        <Box width="50%">
-          <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+          <Box
+            sx={{
+              bgcolor: `primary.${darkTheme ? "dark" : "light"}`,
+              borderRadius: 1,
+              p: 1,
+              mb: 2,
+              display: "flex",
+            }}
+          >
+            <Box width="50%">
+              <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+                <Box
+                  sx={{
+                    background: `url('${pfpURL}') no-repeat center/cover #fff`,
+                    width: 100,
+                    height: 100,
+                    borderRadius: 20,
+                    border: "3px solid black",
+                  }}
+                ></Box>
+                <Typography
+                  sx={{ alignSelf: "center", ml: 1, display: "flex" }}
+                >
+                  {data.first_name} <br />
+                  {data.last_name} <br />
+                  {data.email} <br />
+                  {data.country || "NO COUNTRY"}
+                </Typography>
+                <Tooltip
+                  title="User No."
+                  placement="bottom"
+                  arrow
+                  sx={{ ml: 1 }}
+                >
+                  <Chip
+                    label={`#${data.id}`}
+                    color="primary"
+                    component="span"
+                  />
+                </Tooltip>
+              </Box>
+              <Typography>
+                Good Habits:{" "}
+                <Typography component="span">
+                  {completedHabits.habits.length}
+                </Typography>
+              </Typography>
+              <Typography component="span">
+                Main Goals:{" "}
+                {!data.priority_category_1 &&
+                  !data.priority_category_2 &&
+                  !data.priority_category_3 &&
+                  "None"}{" "}
+              </Typography>
+              {data.priority_category_1 && (
+                <Chip
+                  label={data.priority_category_1}
+                  color="primary"
+                  component="span"
+                  sx={{ ml: 1 }}
+                />
+              )}
+              {data.priority_category_2 && (
+                <Chip
+                  label={data.priority_category_2}
+                  color="primary"
+                  component="span"
+                  sx={{ ml: 1 }}
+                />
+              )}
+              {data?.priority_category_3 && (
+                <Chip
+                  label={data.priority_category_3}
+                  color="primary"
+                  component="span"
+                  sx={{ ml: 1 }}
+                />
+              )}
+              <br />
+              <br />
+              <Typography component="p">{displayBio(data?.bio)}</Typography>
+            </Box>
+
             <Box
+              width="50%"
               sx={{
-                background: `url('${pfpURL}') no-repeat center/cover #fff`,
-                width: 100,
-                height: 100,
-                borderRadius: 20,
-                border: "3px solid black",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
-            ></Box>
-            <Typography sx={{ alignSelf: "center", ml: 1, display: "flex" }}>
-              {userData?.first_name} <br />
-              {userData?.last_name} <br />
-              {userData?.email} <br />
-              {userData?.country || "NO COUNTRY"}
-            </Typography>
-            <Tooltip
-              title="User No."
-              placement="bottom"
-              arrow
-              sx={{ ml: 1 }}
             >
-              <Chip
-                label={`#${userData?.id}`}
-                color="primary"
-                component="span"
-              />
-            </Tooltip>
+              {data?.country && (
+                <Box
+                  component="img"
+                  src={`/flags/${
+                    countryShorthands[
+                      data?.country as keyof typeof countryShorthands
+                    ]
+                  }.svg`}
+                  width={150}
+                  height={150}
+                />
+              )}
+            </Box>
           </Box>
-          <Typography>
-            Good Habits:{" "}
-            <Typography component="span">
-              {completedHabits.habits.length}
-            </Typography>
-          </Typography>
-          <Typography component="span">
-            Main Goals:{" "}
-            {!userData?.priority_category_1 &&
-              !userData?.priority_category_2 &&
-              !userData?.priority_category_3 &&
-              "None"}{" "}
-          </Typography>
-          {userData?.priority_category_1 && (
-            <Chip
-              label={userData.priority_category_1}
-              color="primary"
-              component="span"
-              sx={{ ml: 1 }}
-            />
-          )}
-          {userData?.priority_category_2 && (
-            <Chip
-              label={userData.priority_category_2}
-              color="primary"
-              component="span"
-              sx={{ ml: 1 }}
-            />
-          )}
-          {userData?.priority_category_3 && (
-            <Chip
-              label={userData.priority_category_3}
-              color="primary"
-              component="span"
-              sx={{ ml: 1 }}
-            />
-          )}
-          <br />
-          <br />
-          <Typography component="p">{displayBio(userData?.bio)}</Typography>
-        </Box>
-
-        <Box
-          width="50%"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {userData?.country && (
-            <Box
-              component="img"
-              src={`/flags/${
-                countryShorthands[
-                  userData?.country as keyof typeof countryShorthands
-                ]
-              }.svg`}
-              width={150}
-              height={150}
-            />
-          )}
-        </Box>
-      </Box>
-      <Stack
-        spacing={1}
-        direction="row"
-      >
-        <Button onClick={() => navigate("/settings")}>settings</Button>
-      </Stack>
-    </>
+          <Stack
+            spacing={1}
+            direction="row"
+          >
+            <Button onClick={() => navigate("/settings")}>settings</Button>
+          </Stack>
+        </>
+      )}
+    </Box>
   )
 }
 
