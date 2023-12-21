913700bce058fbb1d6b7e6e2fba038190530e912
lazarevkristijan <lazarevkristijan@gmail.com>
2023-12-21 19:26:15 +0200

    add not found styl

--- client/src/sections/NotFound.tsx
+++ client/src/sections/NotFound.tsx
@@ -1,12 +1,55 @@
-import { Box } from "@mui/material"
+import { Box, Button, Stack, Typography } from "@mui/material"
 import { useDispatch } from "react-redux"
 import { changeLocation } from "../features/bottomNav/bottomNavSlice"
-
+import { useNavigate } from "react-router-dom"
+import HomeIcon from "@mui/icons-material/Home"
+import LoginIcon from "@mui/icons-material/Login"
+import PersonIcon from "@mui/icons-material/Person"
 const NotFound = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   dispatch(changeLocation(0))
 
-  return <Box>NotFound</Box>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography
+        sx={{ mt: 5, mb: 3 }}
+        variant="h3"
+      >
+        Ooops.. Page not found
+      </Typography>
+
+      <Stack gap={2}>
+        <Button
+          onClick={() => navigate("/")}
+          startIcon={<HomeIcon />}
+        >
+          return home?
+        </Button>
+
+        <Button
+          onClick={() => navigate("/login")}
+          startIcon={<LoginIcon />}
+        >
+          to login?
+        </Button>
+
+        <Button
+          onClick={() => navigate("/profile")}
+          startIcon={<PersonIcon />}
+        >
+          to profile?
+        </Button>
+      </Stack>
+    </Box>
+  )
 }
 
 export default NotFound
