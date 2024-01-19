e4eceb06ed8c832f5571992c126dbc052f3c57f0
lazarevkristijan <lazarevkristijan@gmail.com>
2024-01-19 07:43:21 +0100

    fix cors origin

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ const port = process.env.DB_PORT || 3001
 
 app.use(
   cors({
-    origin: "https://rituale.digital",
+    origin: "https://www.rituale.digital",
     methods: "GET,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
