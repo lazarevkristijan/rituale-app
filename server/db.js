39759c7ebff57cede057be70cd437de1f1942f62
lazarevkristijan <lazarevkristijan@gmail.com>
2024-01-29 11:31:56 +0100

    change db connection string

--- server/db.js
+++ server/db.js
@@ -3,13 +3,6 @@ import postgres from "postgres"
 
 dotenv.config()
 
-const sql = postgres({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  ssl: true,
-})
+const sql = postgres(process.env.DB_CONNECTION_STRING)
 
 export default sql
